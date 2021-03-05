85e1d0bd09bfb02c486d997b982d60349db0220e
Frostylele21 <kacp2003@wp.pl>
2021-03-05 15:20:10 +0100

    Java

---
+++ src/ZadanieDrugie.java
@@ -0,0 +1,11 @@
+public class ZadanieDrugie {
+
+    public static void main(String[] args) {
+
+        for (int i = -10; i <= 40; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
